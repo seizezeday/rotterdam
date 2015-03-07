ab9eaad3c45be70355846e71568938a17f9b0041
vasax32 <h7k5@yandex.ua>
2015-03-07 22:24:54 +0200

    timeForTime disabled if not available

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -215,6 +215,19 @@ app.controller('time_tab_controller', function($scope, $http, $filter) {
         {val: '12', description: 'Geen werkdag'}
     ];
 
+    $scope.disabledDayTypes = [
+        {val: '8', description: 'Tijd-voor-tijd'}
+    ];
+
+    $scope.isDisabledDayType = function(el){
+        for(var i in $scope.disabledDayTypes){
+            var entry = $scope.disabledDayTypes[i];
+            if(entry.val == el)
+                return true;
+        }
+        return false;
+    };
+
     $scope.weekTitles = ["Maandag", "Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag"];
 
     $scope.selectedDate = DateTools.convertDateToString(new Date());
@@ -281,6 +294,12 @@ app.controller('time_tab_controller', function($scope, $http, $filter) {
 
         $http.get('api/timeFor').then(function(res){
             $scope.timeForTime = res.data.timeForTime;
+            $scope.disabledDayTypes = [];
+            if($scope.timeForTime < 11){
+                $scope.disabledDayTypes.push(
+                    {val: '8', description: 'Tijd-voor-tijd'}
+                );
+            }
         });
     };
 
@@ -829,4 +848,47 @@ app.controller('LanguageCtrl', function($scope) {
 //  $scope.button = {
 //    radioClient: 0
 //  };
+});
+
+app.directive('optionsDisabled', function($parse) {
+    var disableOptions = function(scope, attr, element, data,
+                                  fnDisableIfTrue) {
+        // refresh the disabled options in the select element.
+        var options = element.find("option");
+        for(var pos= 0,index=0;pos<options.length;pos++){
+            var elem = angular.element(options[pos]);
+            var isDisabled = scope.$parent.$parent.isDisabledDayType(elem.val());
+            if(isDisabled){
+                if(elem.val()!=""){
+                    var locals = {};
+                    locals[attr] = data[index];
+                    elem.attr("disabled", fnDisableIfTrue(scope, locals));
+                    index++;
+                }
+            }
+        }
+    };
+    return {
+        priority: 0,
+        require: 'ngModel',
+        link: function(scope, iElement, iAttrs, ctrl) {
+            // parse expression and build array of disabled options
+            var expElements = iAttrs.optionsDisabled.match(
+                /^\s*(.+)\s+for\s+(.+)\s+in\s+(.+)?\s*/);
+            var attrToWatch = expElements[3];
+            var fnDisableIfTrue = $parse(expElements[1]);
+            scope.$watch(attrToWatch, function(newValue, oldValue) {
+                if(newValue)
+                    disableOptions(scope, expElements[2], iElement,
+                        newValue, fnDisableIfTrue);
+            }, true);
+            // handle model updates properly
+            scope.$watch(iAttrs.ngModel, function(newValue, oldValue) {
+                var disOptions = $parse(attrToWatch)(scope);
+                if(newValue)
+                    disableOptions(scope, expElements[2], iElement,
+                        disOptions, fnDisableIfTrue);
+            });
+        }
+    };
 });
